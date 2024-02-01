f5a6f9a7d73e974560e337cf06e7d7c90ddc24c7
alihantangul <tangulalihan@gmail.com>
2024-02-01 11:13:46 +0300

    İletişim modülü ve admin paneli ayarları yapıldı

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from blog.models import (
-    KategoriModel, YazilarModel, YorumModel
+    KategoriModel, YazilarModel, YorumModel, IletisimModel
 )
 admin.site.register(KategoriModel)
 
@@ -18,6 +18,13 @@ class YorumAdmin(admin.ModelAdmin):
     search_fields = ('yazan_username',)
 
 admin.site.register(YorumModel,YorumAdmin)
+
+class IletisimAdmin(admin.ModelAdmin):
+    list_display = ('email', 'olusturulma_tarihi')
+    search_fields = ('email',)
+
+admin.site.register(IletisimModel,IletisimAdmin)
+
 
 
 
